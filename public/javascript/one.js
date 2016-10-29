aa055c943ea41d4486c196164bac5de5b2e714b1
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-10-29 11:31:22 +0530

    Giving random shade of blue color to random bars.

--- public/javascript/one.js
+++ public/javascript/one.js
@@ -1,4 +1,5 @@
 var data = [20,52,83,23,10,78,14,97,33,40];
+var colors = ["#00004f","#0000ee","#00003f","#000077","#0000dd","#000066","#0000cc","#000088","#000099","#0000aa"]
 
 var loadChart = function() {
 	var chart = d3.select(".chart")
@@ -8,6 +9,7 @@ var loadChart = function() {
 	chart.enter().append("div")
 		.style("width", function(d) {return d*10})
 		.text(function(d) {return d})
+		.style("background-color", function(d){return colors[d%9]})
 
 	chart.exit().remove();
 }
